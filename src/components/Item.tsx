69f681402fe436636d44d2ed245a8d5f23626f8b
widcardw <lui747353367@qq.com>
2022-12-04 18:53:04 +0800

    chore: fade in animation

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import { Show } from 'solid-js'
+import { createSignal } from 'solid-js'
 import type { Component } from 'solid-js'
 import type { DanmuMsg } from 'blive-message-listener'
 
@@ -8,10 +8,18 @@ interface Props {
 }
 
 export const Item: Component<Props> = ({ data, lifeTime }) => {
+  const t = lifeTime || 15000
+  const [animation, setAnimation] = createSignal('0.2s fade-in')
+  setTimeout(() => {
+    setAnimation('')
+  }, 200)
+  setTimeout(() => {
+    setAnimation(`1s fade-out`)
+  }, t - 1000)
+
   return (
     <div 
-      my-3 animate-count-1
-      style={{animation: `1s ease ${(lifeTime || 15000) - 1000}ms fade-out-delay`}}
+      my-3 style={{animation: animation()}}
     >
       <div class="uname" text="#66b395">{ data.user.uname }</div>
       <div class="content" text="truegray-300" mt-2>
